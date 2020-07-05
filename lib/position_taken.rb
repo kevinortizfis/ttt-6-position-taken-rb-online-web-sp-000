46b406ed89d5de270e23a041a9f972cf0cd89532
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-05 17:33:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
 # code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "]
+index = gets.strip.to_i - 1
 def position_taken?(board,index)
   if board[index] == " "
     false
@@ -6,7 +8,8 @@ def position_taken?(board,index)
     false
   elsif board[index] == nil
     false
-  elsif board[index]
+  elsif board[index] == "X"
     true
   end
 end
+puts position_taken?(board,index)
