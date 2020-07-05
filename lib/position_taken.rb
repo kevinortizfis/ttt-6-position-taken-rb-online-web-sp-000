7f6da3e7d4813a16f5d701d61ae065cab516a615
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-05 17:34:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,2 @@
 # code your #position_taken? method here!
-board = [" "," "," "," "," "," "," "," "]
-index = gets.strip.to_i - 1
-def position_taken?(board,index)
-  if board[index] == " "
-    false
-  elsif board[index] == ''
-    false
-  elsif board[index] == nil
-    false
-  elsif board[index] == "X"
-    true
-  end
-end
-puts position_taken?(board,index)
+
